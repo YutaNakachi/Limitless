a8ec3ef5e161d3230cb61de1c797732809283635
YutaNakachi <yuta.nakachi@gmail.com>
2026-04-16 23:40:46 +0900

    入力操作をInputSystemに変更

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,27 +3,57 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D _rigidbody;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 10f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody2D _rigidbody;
+    private Vector2 moveInput;
+    private bool isGrounded;
+
+    void Awake()
     {
-        Application.targetFrameRate = 60;
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        // 入力値を読み込む（Vector2のx成分が左右移動）
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        // ボタンが押された瞬間、かつ接地している場合
+        if (context.started && isGrounded)
+        {
+            _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
     void Update()
     {
-        if (Keyboard.current.dKey.isPressed)
-        {
-            _rigidbody.linearVelocity = Vector2.right * 5f;
-        }
 
-        if (Keyboard.current.aKey.isPressed)
-        {
-            _rigidbody.linearVelocity = Vector2.left * 5f;
+    }
 
-        }
+    void FixedUpdate()
+    {
+        // 左右移動の適用
+        _rigidbody.linearVelocity = new Vector2(moveInput.x * moveSpeed, _rigidbody.linearVelocity.y);
+    }
+
+    // 地面判定（簡易版）
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
     }
 }
