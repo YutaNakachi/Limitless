569c2bc3c51f70dda8f7c109094a09d519305df7
YutaNakachi <yuta.nakachi@gmail.com>
2026-04-26 17:23:00 +0900

    Shoot機能と画面端の壁実装
    Winformから基本機能移植完了

--- Assets/Scripts/Controllers/PlayerShoot.cs
+++ Assets/Scripts/Controllers/PlayerShoot.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
-    [SerializeField] private float shootForce = 15f;
     [SerializeField] private Collider2D shootCollider;
+    [SerializeField] private float shootForce = 15f;
+    [SerializeField] private float angle = 45.0f; // 飛ばしたい角度（度）
+    private float rad => angle * Mathf.Deg2Rad; // 度をラジアンに変換
 
     public void OnShootStart()
     {
@@ -16,7 +18,13 @@ public class PlayerShoot : MonoBehaviour
 
         BallController ballInRange = collider.GetComponent<BallController>();
 
-        Vector2 direction = (ballInRange.transform.position - transform.position).normalized;
+        //Vector2 direction = (ballInRange.transform.position - transform.position).normalized;
+
+        // プレイヤーよりボールが左なら -1、右なら 1 を掛ける
+        float side = (ballInRange.transform.position.x < transform.position.x) ? -1f : 1f;
+        // X = Cos, Y = Sin で方向ベクトルを作る
+        Vector3 direction = new Vector3(Mathf.Cos(rad) * side, Mathf.Sin(rad));
+
         ballInRange.ShotBall(direction, shootForce);
 
         Debug.Log("ナイスシュート！");
