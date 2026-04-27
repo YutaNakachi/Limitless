eea7cb1b479666a39030e90df48374e302aa4e81
YutaNakachi2026 <uni202601_07@telework.to>
2026-04-27 15:54:30 +0900

    InputSystemのRe-Import

--- Assets/Scripts/Generators/BlockGenerator.cs
+++ Assets/Scripts/Generators/BlockGenerator.cs
@@ -7,7 +7,7 @@ public class BlockGenerator : ObjectGenerator
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Vector3 blockPosition_leftSide = new Vector3(Random.Range(-7.0f, -2.0f), Random.Range(-3.0f, -1.0f));
+        Vector3 blockPosition_leftSide = new Vector3(Random.Range(-7.0f, -2.0f), Random.Range(0.0f, 3.0f));
         Vector3 blockPosition_rightSide = new Vector3(Random.Range(2.0f, 7.0f), Random.Range(0.0f, 3.0f));
         GenerateObject(blockPrefab, blockPosition_leftSide);
         GenerateObject(blockPrefab, blockPosition_rightSide);
