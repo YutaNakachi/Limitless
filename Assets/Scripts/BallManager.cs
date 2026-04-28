367f83856a95d501a7f9f7a12f47acc17e3df1d2
YutaNakachi <yuta.nakachi@gmail.com>
2026-04-29 01:06:40 +0900

    Playerの周りをBallが回わる仕組みを実装

--- Assets/Scripts/BallManager.cs
+++ Assets/Scripts/BallManager.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class BallManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody2D _rigidbody;
+    private BallController _ball;
+
+    [SerializeField] PlayerController _playerController;
+
+    [SerializeField] private float orbitRadius = 2f;
+    [SerializeField] private float rotateSpeed = 100f;
+    private float currentAngle;
+
+    void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _ball = GetComponent<BallController>();
+        GetComponent<Collider2D>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentAngle -= rotateSpeed * Time.deltaTime;
+        UpdateBallPositions();
+    }
+    void UpdateBallPositions()
+    {
+        // 各ボールの角度を均等に割り振る（360度 / ボール数）
+
+        float rad = currentAngle * Mathf.Deg2Rad;
+
+        Vector3 offset = new Vector3(Mathf.Cos(rad), Mathf.Sin(rad), 0) * orbitRadius;
+        Orbit(_playerController.transform.position + offset + new Vector3(0, _playerController.GetComponent<CapsuleCollider2D>().size.y / 2 - 0.2f, 0));
+    }
+
+    public void Orbit(Vector3 targetPosition)
+    {
+        transform.position = targetPosition;
     }
 }
